5ad2a0300b10923582a0f74cc9ec461221c7ca1c
Lorenz Fuentes <fuentes2231163@mls.ceu.edu.ph>
2024-11-05 11:17:43 +0800

    nxiqnwxonwqkxnqwlkpxnmwqikpncxkqwpmxpkqwncxokwnmc

--- index.js
+++ index.js
@@ -10,64 +10,13 @@ app.listen(PORT, () => {
 });
 
 // Customers
-app.get('/customers', (request, response) => {
-    response.status(200).json({ message: 'GET all customers' });
-});
-
-app.post('/customers', (request, response) => {
-    response.status(200).json({ message: `CREATE new customer` });
-});
-
-app.get('/customers/:id', (request, response) => {
-    response.status(200).json({ message: `GET customer with id ${request.params.id}` });
-});
-
-app.put('/customers/:id', (request, response) => {
-    response.status(200).json({ message: `UPDATE customer with id ${request.params.id}` });
-});
-
-app.delete('/customers/:id', (request, response) => {
-    response.status(200).json({ message: `DELETE customer with id ${request.params.id}` });
-});
+const customerRouter = require('./routes/customer');
+app.use('/customers', customerRouter);
 
 // Orders
-app.get('/orders', (request, response) => {
-    response.status(200).json({ message: 'GET all orders' });
-});
-
-app.post('/orders', (request, response) => {
-    response.status(200).json({ message: `CREATE new order` });
-});
-
-app.get('/orders/:id', (request, response) => {
-    response.status(200).json({ message: `GET order with id ${request.params.id}` });
-});
-
-app.put('/orders/:id', (request, response) => {
-    response.status(200).json({ message: `UPDATE order with id ${request.params.id}` });
-});
-
-app.delete('/orders/:id', (request, response) => {
-    response.status(200).json({ message: `DELETE order with id ${request.params.id}` });
-});
+const orderRouter = require('./routes/order');
+app.use('/orders', orderRouter);
 
 // Payments
-app.get('/payments', (request, response) => {
-    response.status(200).json({ message: 'GET all payments' });
-});
-
-app.post('/payments', (request, response) => {
-    response.status(200).json({ message: `CREATE new payments` });
-});
-
-app.get('/payments/:id', (request, response) => {
-    response.status(200).json({ message: `GET payments with id ${request.params.id}` });
-});
-
-app.put('/payments/:id', (request, response) => {
-    response.status(200).json({ message: `UPDATE payments with id ${request.params.id}` });
-});
-
-app.delete('/payments/:id', (request, response) => {
-    response.status(200).json({ message: `DELETE payments with id ${request.params.id}` });
-});
+const paymentRouter = require('./routes/payment');
+app.use('/payments', paymentRouter);
